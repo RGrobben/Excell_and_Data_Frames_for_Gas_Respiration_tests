98fc7f3af18aa887a51913d183c48b82ab146ae0
Robbert Grobben <robbert.grobben@bam.com>
2023-06-15 12:27:03 +0200

    check function written for ConstantsSample

--- data_classes.py
+++ data_classes.py
@@ -14,48 +14,71 @@ class ConstantsSample:
     henryeff_20: float = 5.23 ** -3
 
     @property
-    def check_all_constants_filled(self):
-        pass
+    def check_all_checks(self):
+        checks = [
+            self.check_Rgas,
+            self.check_expTemp,
+            self.check_volume_headspace,
+            self.check_water_volume,
+            self.check_dry_mass_sample,
+            self.check_molar_mass_carbon,
+        ]
+        error_messages = []
+
+        for check in checks:
+            if check is not True:
+                error_messages.append(check)
+
+        if not error_messages:
+            return True
+        else:
+            return error_messages
 
     @property
     def check_Rgas(self):
         check_if_float = isinstance(self.Rgas, float)
-        return check_if_float
+        if not check_if_float:
+            return "Rgas is not a float"
+        return True
 
     @property
     def check_expTemp(self):
         check_if_float = isinstance(self.expTemp, float)
-        return check_if_float
+        if not check_if_float:
+            return "expTemp is not a float"
+        return True
 
     @property
     def check_volume_headspace(self):
         check_if_float = isinstance(self.volume_headspace, float)
-        return check_if_float
+        if not check_if_float:
+            return "volume_headspace is not a float"
+        return True
 
     @property
     def check_water_volume(self):
         check_if_float = isinstance(self.water_volume, float)
-        return check_if_float
+        if not check_if_float:
+            return "water_volume is not a float"
+        return True
 
     @property
     def check_dry_mass_sample(self):
         check_if_float = isinstance(self.dry_mass_sample, float)
-        return check_if_float
+        if not check_if_float:
+            return "dry_mass_sample is not a float"
+        return True
 
     @property
     def check_molar_mass_carbon(self):
         check_if_float = isinstance(self.molar_mass_carbon, float)
         check_if_float_is_oke = False
-        if self.molar_mass_carbon == 12 or 12.011:
+        if self.molar_mass_carbon == 12 or self.molar_mass_carbon == 12.011:
             check_if_float_is_oke = True
 
-        if check_if_float and check_if_float_is_oke:
-            return True
-
-    @property
-    def check_henryeff_20(self):
-        check_if_float = isinstance(self.molar_mass_carbon, float)
-        return check_if_float
+        if not check_if_float or check_if_float_is_oke:
+            return "molar_mass_carbon is not correct"
+        return True
 
 
 @dataclass
---
+++ test/test_data_classes.py
@@ -0,0 +1,79 @@
+import unittest
+
+from data_classes import ConstantsSample
+
+
+class ConstantsSampleTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.constants = ConstantsSample(
+            Rgas=8.314,
+            expTemp=300.0,
+            volume_headspace=0.5,
+            water_volume=10.0,
+            dry_mass_sample=5.0
+        )
+
+    def test_check_all_checks(self):
+        self.assertTrue(self.constants.check_all_checks)
+
+    def test_check_Rgas(self):
+        self.assertTrue(self.constants.check_Rgas)
+
+    def test_check_expTemp(self):
+        self.assertTrue(self.constants.check_expTemp)
+
+    def test_check_volume_headspace(self):
+        self.assertTrue(self.constants.check_volume_headspace)
+
+    def test_check_water_volume(self):
+        self.assertTrue(self.constants.check_water_volume)
+
+    def test_check_dry_mass_sample(self):
+        self.assertTrue(self.constants.check_dry_mass_sample)
+
+    def test_check_all_checks_incorrect(self):
+        constants = self.constants
+        constants.Rgas = "invalid"
+        constants.expTemp = [1, 2, 3]
+        constants.volume_headspace = "invalid"
+        constants.water_volume = None
+        constants.dry_mass_sample = True
+
+        all_checks = constants.check_all_checks
+        self.assertIsInstance(all_checks, list)
+        self.assertIsInstance(all_checks[0], str)
+
+    def test_check_Rgas_incorrect(self):
+        # Invalid value: Rgas is a string instead of a float
+        constants = self.constants
+        constants.Rgas = "invalid"
+        self.assertEqual(constants.check_Rgas, "Rgas is not a float")
+
+    def test_check_expTemp_incorrect(self):
+        # Invalid value: expTemp is a list instead of a float
+        constants = self.constants
+        constants.expTemp = [1, 2, 3]
+        self.assertEqual(constants.check_expTemp, "expTemp is not a float")
+
+    def test_check_volume_headspace_incorrect(self):
+        # Invalid value: volume_headspace is a string instead of a float
+        constants = self.constants
+        constants.volume_headspace = "invalid"
+        self.assertEqual(constants.check_volume_headspace, "volume_headspace is not a float")
+
+    def test_check_water_volume_incorrect(self):
+        # Invalid value: water_volume is None instead of a float
+        constants = self.constants
+        constants.water_volume = None
+        self.assertEqual(constants.check_water_volume, "water_volume is not a float")
+
+    def test_check_dry_mass_sample_incorrect(self):
+        # Invalid value: dry_mass_sample is a boolean instead of a float
+        constants = self.constants
+        constants.dry_mass_sample = True
+        self.assertEqual(constants.check_dry_mass_sample, "dry_mass_sample is not a float")
+
+
+if __name__ == '__main__':
+    unittest.main()
