eb10ecde30494319d5c002764adca3cc75299cc1
Robbert Grobben <robbert.grobben@bam.com>
2023-07-02 21:29:02 +0200

    written docstrings

--- data_frame_processor.py
+++ data_frame_processor.py
@@ -4,17 +4,21 @@ from typing import Any
 
 class DataFrameProcessor:
     """
-    A class for processing pandas DataFrames standard operations.
+    A class for processing pandas DataFrames with standard operations.
     """
 
     @staticmethod
-    def fill_nan_values(data_frame: pd.DataFrame, column_name: str, value: Any) -> None:
+    def fill_nan_values(data_frame: pd.DataFrame, column_name: str, value: float) -> None:
         """
         Fill the NaN values in a column with a specified value.
 
-        :param data_frame:
-        :param column_name: The name of the column to fill NaN values.
-        :param value: The value to fill NaN values.
+        Parameters:
+            - data_frame (pd.DataFrame): The DataFrame to modify.
+            - column_name (str): The name of the column to fill NaN values.
+            - value (float): The value to fill NaN values.
+
+        Returns:
+            None
         """
         data_frame[column_name] = data_frame[column_name].fillna(value)
 
@@ -22,6 +26,26 @@ class DataFrameProcessor:
     def add_day_column(data_frame: pd.DataFrame, date_column_name: str,
                        time_column_name: str, day_plus_time_column_name: str = "Day + Time",
                        day_column_name: str = "Day", dayfirst: bool = False) -> None:
+        """
+            Add a new column to the DataFrame representing the number of days since the first row's date and time.
+
+            Parameters:
+                - data_frame (pd.DataFrame): The DataFrame to modify.
+                - date_column_name (str): The name of the column containing the date values.
+                - time_column_name (str): The name of the column containing the time values.
+                - day_plus_time_column_name (str, optional): The name of the new column combining date and time values. Defaults to "Day + Time".
+                - day_column_name (str, optional): The name of the new column representing the number of days. Defaults to "Day".
+                - dayfirst (bool, optional): Whether the date format is day first (e.g., "25-12-2022" for December 25th, 2022). Defaults to False.
+
+            Returns:
+                None
+
+            Note:
+                - The method converts the date and time columns to string format.
+                - It combines the date and time values into a new column named "Day + Time" using the pd.to_datetime() function.
+                - It calculates the number of days since the first row's date and time and assigns the values to a new column named "Day".
+                - The calculation is based on the difference between each row's "Day + Time" value and the first row's "Day + Time" value.
+            """
         data_frame[date_column_name] = data_frame[date_column_name].astype(str)
         data_frame[time_column_name] = data_frame[time_column_name].astype(str)
 
@@ -35,14 +59,15 @@ class DataFrameProcessor:
     def replace_position_column(data_frame: pd.DataFrame, name_replaced_column: str,
                                 name_column_of_position: str) -> None:
         """
-        Replaces a column in a the DataFrame at the left of the location of column that is named as the desired location.
+        Replaces a column in the DataFrame at the left of the location of the column named as the desired location.
 
-        Args:
-        - name_replaced_column: The name of the column to be replaced.
-        - name_column_of_position: The name of the column where the replaced column should be positioned.
+        Parameters:
+        - data_frame (pd.DataFrame): The DataFrame to modify.
+        - name_replaced_column (str): The name of the column to be replaced.
+        - name_column_of_position (str): The name of the column where the replaced column should be positioned.
 
         Returns:
-        - None
+        None
         """
         column_to_be_replaced = data_frame.pop(name_replaced_column)
         data_frame.insert(loc=data_frame.columns.get_loc(name_column_of_position),
