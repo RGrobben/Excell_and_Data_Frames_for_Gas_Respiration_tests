a8257399d99f640db8a9eaff9986ce072d611fc9
Robbert Grobben <robbert.grobben@bam.com>
2023-07-02 21:45:08 +0200

    written docstrings

--- data_frame_calculations_standard_for_gas_respiration_tests.py
+++ data_frame_calculations_standard_for_gas_respiration_tests.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class GasComposition:
     """
-    A class for setting the gas composition in the pandas DataFrame.
+    A class for the gas composition.
     """
 
     @staticmethod
@@ -13,19 +13,28 @@ class GasComposition:
                             name_column_o2: str = "O2 [%]", name_column_n2: str = "N2 [%]",
                             index: int = 0) -> None:
         """
-        Set the values of CH4, CO2, O2 and N2 in the DataFrame.
+            Set the values of CH4, CO2, O2, and N2 in the DataFrame.
 
-        :param name_column_ch4:
-        :param name_column_co2:
-        :param name_column_n2:
-        :param name_column_o2:
-        :param data_frame: The data frame to do the operation for.
-        :param ch4: The value of CH4 in [%].
-        :param co2: The value of CO2 in [%].
-        :param o2: The value of O2 in [%].
-        :param n2: The value of N2 in [%].
-        :param index: The index of the row to set the values. Default is 0.
-        """
+            Parameters:
+                - data_frame (pd.DataFrame): The DataFrame to modify.
+                - ch4 (float): The value of CH4 in [%].
+                - co2 (float): The value of CO2 in [%].
+                - o2 (float): The value of O2 in [%].
+                - n2 (float): The value of N2 in [%].
+                - name_column_ch4 (str, optional): The name of the column for CH4. Defaults to "CH4 [%]".
+                - name_column_co2 (str, optional): The name of the column for CO2. Defaults to "CO2 [%]".
+                - name_column_o2 (str, optional): The name of the column for O2. Defaults to "O2 [%]".
+                - name_column_n2 (str, optional): The name of the column for N2. Defaults to "N2 [%]".
+                - index (int, optional): The index of the row to set the values. Defaults to 0.
+
+            Returns:
+                None
+
+            Example:
+                my_data_frame = pd.DataFrame(...)
+                NicePandaFrameFunctions.set_gas_composition(data_frame=my_data_frame, ch4=10.5, co2=20.3, o2=5.7, n2=63.5)
+                # The CH4, CO2, O2, and N2 values are set in the specified columns and row of the DataFrame.
+            """
 
         data_frame.loc[index, [name_column_ch4, name_column_co2, name_column_o2, name_column_n2]] = [ch4, co2, o2, n2]
 
@@ -41,7 +50,34 @@ class GasComposition:
                         name_correction_n2: str = "N2-corr [%]",
                         ) -> None:
         """
-        Corrects the measuring values so that the sum is 100% in the DataFrame.
+        Correct the measuring values in the DataFrame so that the sum is 100%.
+
+        Parameters:
+        - data_frame (pd.DataFrame): The DataFrame to modify.
+        - name_column_ch4 (str, optional): The name of the column for CH4. Defaults to "CH4 [%]".
+        - name_column_co2 (str, optional): The name of the column for CO2. Defaults to "CO2 [%]".
+        - name_column_o2 (str, optional): The name of the column for O2. Defaults to "O2 [%]".
+        - name_column_n2 (str, optional): The name of the column for N2. Defaults to "N2 [%]".
+        - name_column_summation (str, optional): The name of the column for the summation of measuring values.
+        Defaults to "Sum [%]".
+        - name_column_summation_correction (str, optional): The name of the column for the corrected summation
+        of measuring values. Defaults to "Sum-corr [%]".
+        - name_correction_ch4 (str, optional): The name of the column for the corrected value of CH4.
+        Defaults to "CH4-corr [%]".
+        - name_correction_co2 (str, optional): The name of the column for the corrected value of CO2.
+        Defaults to "CO2-corr [%]".
+        - name_correction_o2 (str, optional): The name of the column for the corrected value of O2.
+        Defaults to "O2-corr [%]".
+        - name_correction_n2 (str, optional): The name of the column for the corrected value of N2.
+        Defaults to "N2-corr [%]".
+
+        Returns:
+        None
+
+        Example:
+        my_data_frame = pd.DataFrame(...)
+        NicePandaFrameFunctions.sum_correct_sum(data_frame=my_data_frame)
+        # The measuring values are corrected so that the sum is 100% in the DataFrame.
         """
         df = data_frame
 
@@ -102,10 +138,57 @@ class MolGasCompositionCalculations:
                                               name_column_mg_before_or_after: str,
                                               name_column_specific_gas_corrected: str
                                               ) -> None:
+        """
+           Calculate the amount of a specific gas in moles before sampling in the DataFrame.
+
+           Parameters:
+               - data_frame (pd.DataFrame): The DataFrame to modify.
+               - name_column (str): The name of the column to store the calculated amount of the specific gas in moles.
+               - name_column_mg_before_or_after (str): The name of the column containing the mass of the specific gas
+               (before or after sampling).
+               - name_column_specific_gas_corrected (str): The name of the column containing the corrected value of the
+               specific gas.
+
+           Returns:
+               None
+
+           Example:
+               my_data_frame = pd.DataFrame(...)
+               NicePandaFrameFunctions.specific_gas_in_moles_before_sampling(
+                   data_frame=my_data_frame,
+                   name_column='Moles Before Sampling',
+                   name_column_mg_before_or_after='Mass Before Sampling',
+                   name_column_specific_gas_corrected='Specific Gas Corrected'
+               )
+               # The DataFrame now contains a new column 'Moles Before Sampling' representing the amount of a
+               specific gas in moles before sampling.
+           """
         data_frame[name_column] = data_frame[name_column_mg_before_or_after] * \
                                   data_frame[name_column_specific_gas_corrected] * (1 / 100)
 
     @staticmethod
     def carbon_total_moles(data_frame: pd.DataFrame, name_column: str, name_column_CO2: str,
                            name_column_CH4: str) -> None:
+        """
+          Calculate the total moles of carbon in the DataFrame.
+
+          Parameters:
+              - data_frame (pd.DataFrame): The DataFrame to modify.
+              - name_column (str): The name of the column to store the calculated total moles of carbon.
+              - name_column_CO2 (str): The name of the column containing the moles of CO2.
+              - name_column_CH4 (str): The name of the column containing the moles of CH4.
+
+          Returns:
+              None
+
+          Example:
+              my_data_frame = pd.DataFrame(...)
+              NicePandaFrameFunctions.carbon_total_moles(
+                  data_frame=my_data_frame,
+                  name_column='Total Moles of Carbon',
+                  name_column_CO2='CO2 Moles',
+                  name_column_CH4='CH4 Moles'
+              )
+              # The DataFrame now contains a new column 'Total Moles of Carbon' representing the total moles of carbon.
+          """
         data_frame[name_column] = data_frame[name_column_CO2] + data_frame[name_column_CH4]
