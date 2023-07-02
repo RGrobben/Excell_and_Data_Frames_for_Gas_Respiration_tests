18aad88c83c5f29705e8e9f6d157b6e448a7644e
Robbert Grobben <robbert.grobben@bam.com>
2023-07-02 21:40:40 +0200

    written docstrings

--- nice_functions.py
+++ nice_functions.py
@@ -7,15 +7,41 @@ from openpyxl.utils import get_column_letter
 
 
 class NiceExcelFunction:
+    """ A class with nice functions for Excel"""
     @staticmethod
-    def get_column_index_from_letter(column_letter):
+    def get_column_index_from_letter(column_letter) -> float | int:
+        """
+            Get the column index of Excel from a given column letter.
+
+            Parameters:
+                - column_letter (str): The column letter to convert to an index.
+
+            Returns:
+                int: The corresponding column index.
+            Example:
+                column_index = WorkbookLoader.get_column_index_from_letter('C')
+                # The column_index variable will contain the value 3.
+            """
         index = 0
         for char in column_letter:
             index = index * 26 + (ord(char.upper()) - ord('A')) + 1
         return index
 
     @staticmethod
-    def get_column_letter_from_index(index):
+    def get_column_letter_from_index(index) -> str:
+        """
+            Get the column letter of Excel from a given column index.
+
+            Parameters:
+                - index (int): The column index to convert to a letter.
+
+            Returns:
+                str: The corresponding column letter.
+
+            Example:
+                column_letter = WorkbookLoader.get_column_letter_from_index(3)
+                # The column_letter variable will contain the value 'C'.
+            """
         letters = ''
         while index:
             index, remainder = divmod(index - 1, 26)
@@ -64,6 +90,21 @@ class NiceExcelFunction:
                                                                               search_string: str,
                                                                               header_row: int,
                                                                               ) -> str:
+        """
+            Find the Excel column name (letter) based on the column name string in the given header row.
+
+            Parameters:
+                - workbook (Workbook): The workbook containing the sheet.
+                - sheet_name (str): The name of the sheet in which to search.
+                - search_string (str): The column name string to search for in the header row.
+                - header_row (int): The row index of the header row.
+
+            Returns:
+                str: The Excel column name (letter) corresponding to the column name string.
+
+            Raises:
+                ValueError: If the search string is not found in the header row.
+            """
 
         # Load the workbook and select the sheet
         sheet = workbook[sheet_name]
@@ -84,13 +125,47 @@ class NiceExcelFunction:
         raise ValueError(f"Search string '{search_string}' not found in the header row.")
 
     @staticmethod
-    def get_cell_value_pandas(file_path, sheet_name, cell):
+    def get_cell_value_pandas(file_path, sheet_name, cell) -> float | int:
+        """
+            Get the value of a specific cell in an Excel file using pandas.
+
+            Parameters:
+                - file_path (str): The path to the Excel file.
+                - sheet_name (str): The name of the sheet containing the cell.
+                - cell (str): The cell reference (e.g., 'A1', 'B2').
+
+            Returns:
+                object: The value of the specified cell.
+
+            Example:
+                cell_value = WorkbookLoader.get_cell_value_pandas(file_path='path/to/excel_file.xlsx',
+                                                                 sheet_name='Sheet1',
+                                                                 cell='A1')
+                # The cell_value variable will contain the value of cell A1 in Sheet1.
+            """
         df = pd.read_excel(file_path, sheet_name=sheet_name)
         value = df.loc[cell].values[0]
         return value
 
     @staticmethod
-    def get_cell_value_openpyxl(file_path, sheet_name, cell):
+    def get_cell_value_openpyxl(file_path, sheet_name, cell) -> float | int:
+        """
+            Get the value of a specific cell in an Excel file using openpyxl.
+
+            Parameters:
+                - file_path (str): The path to the Excel file.
+                - sheet_name (str): The name of the sheet containing the cell.
+                - cell (str): The cell reference (e.g., 'A1', 'B2').
+
+            Returns:
+                object: The value of the specified cell.
+
+            Example:
+                cell_value = WorkbookLoader.get_cell_value_openpyxl(file_path='path/to/excel_file.xlsx',
+                                                                    sheet_name='Sheet1',
+                                                                    cell='A1')
+                # The cell_value variable will contain the value of cell A1 in Sheet1.
+            """
         wb = load_workbook(filename=file_path)
         sheet = wb[sheet_name]
         value = sheet[cell].value
@@ -98,8 +173,24 @@ class NiceExcelFunction:
 
 
 class NicePandaFrameFunctions:
-
+    """ A class with nice functions for panda data frames"""
     @staticmethod
     def get_cell_value_pandas_df(dataframe: pd.DataFrame, row: int, column: int) -> str | float:
+        """
+            Get the value of a specific cell in a pandas DataFrame.
+
+            Parameters:
+                - dataframe (pd.DataFrame): The DataFrame containing the cell.
+                - row (int): The row index of the cell.
+                - column (int): The column index of the cell.
+
+            Returns:
+                str or float: The value of the specified cell.
+
+            Example:
+                my_dataframe = pd.DataFrame(...)
+                cell_value = NicePandaFrameFunctions.get_cell_value_pandas_df(dataframe=my_dataframe, row=0, column=0)
+                # The cell_value variable will contain the value of the cell at row 0, column 0 in the DataFrame.
+            """
         value = dataframe.loc[row:column]
         return value
